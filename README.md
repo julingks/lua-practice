0db345f470ac01cb0c65fe3888060a4db9dd7360
Minwoo Kim <minwoo.kim@devsisters.com>
2015-04-14 11:58:54 +0900

    modify README.md

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Study Material
 * http://www.lua.org/pil/1.html
 
+## Go compiler 
+* https://github.com/yuin/gopher-lua
---
+++ gopherlua.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/yuin/gopher-lua"
+
+func main() {
+	L := lua.NewState()
+	defer L.Close()
+	if err := L.DoFile("hello.lua"); err != nil {
+		panic(err)
+	}
+}
